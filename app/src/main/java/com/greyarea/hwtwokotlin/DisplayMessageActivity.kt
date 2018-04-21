bd1939d15deb12aa0faea53ddcd14316c9996736
greystar <lyndonp80@gmail.com>
2018-04-21 09:35:42 -0700

    Buttons and Toasts created

--- app/src/main/java/com/greyarea/hwtwokotlin/DisplayMessageActivity.kt
+++ app/src/main/java/com/greyarea/hwtwokotlin/DisplayMessageActivity.kt
@@ -89,4 +89,3 @@ class DisplayMessageActivity : AppCompatActivity() {
         }
     }
 }
-
--- app/src/main/java/com/greyarea/hwtwokotlin/MainActivity.kt
+++ app/src/main/java/com/greyarea/hwtwokotlin/MainActivity.kt
@@ -5,8 +5,12 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.Toast
+import com.greyarea.hwtwokotlin.R.id.editText
+
 
 const val EXTRA_MESSAGE = "com.greyarea.hwtwokotlin.MESSAGE"
+//private Button button  ;
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,4 +28,23 @@ class MainActivity : AppCompatActivity() {
         }
         startActivity(intent)
     }
+
+    fun toastMsg(msg: String) {
+        val toast = Toast.makeText(this, msg, Toast.LENGTH_SHORT)
+        toast.show()
+    }
+
+    fun displayToastMsgTwo(v: View) {
+        toastMsg("Button Two Clicked")
+    }
+
+    fun displayToastMsgThree(v: View) {
+        toastMsg("Button Three Clicked")
+    }
+
+    fun displayToastMsgFour(v: View) {
+        toastMsg("Button Four Clicked")
+    }
+
+
 }
