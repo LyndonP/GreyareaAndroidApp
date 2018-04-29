617f4150403faaa4df7c288a151e177ad912bf1c
greystar <lyndonp80@gmail.com>
2018-04-29 06:42:39 -0700

    Commit for HW4 - TODO:ExtraCredit

---
+++ app/src/main/java/com/greyarea/grey/greyareaad340/AboutActivity.java
@@ -0,0 +1,28 @@
+package com.greyarea.grey.greyareaad340;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        Toolbar mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+
+    }
+
+}
--- app/src/main/java/com/greyarea/grey/greyareaad340/TextBoxMsgSend.java
+++ app/src/main/java/com/greyarea/grey/greyareaad340/TextBoxMsgSend.java
@@ -3,6 +3,7 @@ package com.greyarea.grey.greyareaad340;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.widget.TextView;
 
@@ -15,6 +16,11 @@ public class TextBoxMsgSend extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_box_msg_send);
+
+        Toolbar sToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(sToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
 
         receive = (TextView)findViewById(R.id.textView1);
 
