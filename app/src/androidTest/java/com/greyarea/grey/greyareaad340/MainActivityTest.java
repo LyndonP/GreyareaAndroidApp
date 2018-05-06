12a473d0c6be5bd2a3d907d5d4fa40a1094a63e6
greystar <lyndonp80@gmail.com>
2018-05-05 20:13:17 -0700

    UI testing working - HW5

--- app/src/androidTest/java/com/greyarea/grey/greyareaad340/MainActivityTest.java
+++ app/src/androidTest/java/com/greyarea/grey/greyareaad340/MainActivityTest.java
@@ -2,35 +2,63 @@ package com.greyarea.grey.greyareaad340;
 
 import android.app.Activity;
 import android.app.Instrumentation;
+import android.content.Context;
+import android.content.Intent;
+import android.preference.PreferenceManager;
 import android.support.test.espresso.Espresso;
 import android.support.test.rule.ActivityTestRule;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+
 
 import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.clearText;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.core.IsNot.not;
 import static org.junit.Assert.*;
+
+import android.content.SharedPreferences;
+
 
 public class MainActivityTest {
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
 
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class, true,
+            false); // Activity is not launched immediately
+
     private MainActivity mActivity = null;
     private String mField = " ";
+    private Intent intent;
+    private String xInput = "T";
+    private SharedPreferences.Editor preferencesEditor;
 
     Instrumentation.ActivityMonitor monitor = getInstrumentation().addMonitor(TextBoxMsgSend.class.getName(), null, false);
 
     @Before
     public void setUp() throws Exception {
         mActivity = mActivityTestRule.getActivity();
+
+        intent = new Intent();
+        Context context = getInstrumentation().getTargetContext();
+
+        // create a SharedPreferences editor
+        preferencesEditor = PreferenceManager.getDefaultSharedPreferences(context).edit();
+
+
     }
 
     //UI tests for text field & button
@@ -47,7 +75,6 @@ public class MainActivityTest {
 
     }
 
-    //UI tests for text field & button
     @Test
     public void testUserInputScenario() {
 
@@ -57,8 +84,37 @@ public class MainActivityTest {
         Espresso.closeSoftKeyboard();
         //perform button click
         Espresso.onView(withId(R.id.button1)).perform(click());
-        //checking the text in the text view
-        Espresso.onView(withId(R.id.editText1)).check(matches(withText(mField)));
+
+    }
+
+
+    // Unit tests for validation logic
+    @Test
+    public void testEditTextBoxForValidEntries() {
+
+        assertFalse(mField.isEmpty());
+        // clears the Edit Text entry box
+        Espresso.onView(withId(R.id.editText1)).perform(clearText());
+        // test to see if blank entry is permitted (expected toast)
+        Espresso.onView(withId(R.id.button1)).perform(click());
+
+    }
+
+
+    // Tests for data storage & retrieval
+    @Test
+    public void populateUsernameFromSharedPrefsTest() {
+        String testUsername = "test";
+
+        // Set SharedPreferences data
+        preferencesEditor.putString("test1", testUsername);
+        preferencesEditor.commit();
+
+        // Launch activity
+        mActivityRule.launchActivity(intent);
+
+        onView(withId(R.id.editText1))
+                .check(matches(isDisplayed()));
 
     }
 
